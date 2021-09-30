2efb240cbc51eb71c21aee940760acfacdde344b
idanacumen <idan@acumen.io>
2021-09-30 09:09:10 +0300

    compare developers in companies' projects

--- code/queries/developer_metrics_by_affiliation.sql
+++ code/queries/developer_metrics_by_affiliation.sql
@@ -93,3 +93,41 @@ order by
 count(distinct d.author_email) desc
 ;
 
+SELECT
+case
+when author_email_domain in
+('google.com', 'redhat.com', 'microsoft.com', 'apache.org', 'intel.com',  'fb.com', '	googlemail.com', 'us.ibm.com', 'apple.com'
+, 'amazon.com') then 'prestigious_conmpany'
+
+when author_email_domain in
+('mit.edu', 'stanford.edu', 'umich.edu', 'inria.fr', 'gatech.edu', 'cornell.edu') then 'prestigious_university'
+else 'other'
+end as prestigious
+, count(*) as cases
+, count(distinct d.author_email) as developers
+
+, avg(files_owned_ccp ) as ccp
+, avg(avg_coupling_code_size_cut ) as avg_coupling_code_size_cut
+, avg(tests_presence ) as tests_presence
+, avg(multiline_message_ratio ) as multiline_message_ratio
+, avg(message_length_avg ) as message_length_avg_ratio
+, avg(same_date_duration_avg ) as same_date_duration_avg
+, avg(files_edited ) as files_edited
+, avg(refactor_mle ) as refactor_mle
+, avg(commit_days ) as commit_days
+, avg(one_file_fix_rate ) as one_file_fix_rate
+, avg(one_file_refactor_rate ) as one_file_refactor_rate
+
+from general.companies as c
+join
+general.developer_per_repo_profile as d
+on
+regexp_contains(d.repo_name, concat('^', c.user))
+where Company in ('ibm', 'Apple', 'Amazon', 'Google', 'Microsoft', 'Facebook')
+and
+commits >= 200
+group by
+prestigious
+order by
+count(distinct d.author_email) desc
+;
